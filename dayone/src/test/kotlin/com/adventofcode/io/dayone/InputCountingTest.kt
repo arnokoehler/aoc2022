b4764a302b4418113e5361252913732fc3e0490f
Arno Daniel Koehler <arno.koehler@schiphol.nl>
2022-12-02 16:02:57 +0100

    Day 2 part two refactor

--- dayone/src/test/kotlin/com/adventofcode/io/dayone/InputCountingTest.kt
+++ dayone/src/test/kotlin/com/adventofcode/io/dayone/InputCountingTest.kt
@@ -24,4 +24,26 @@ class InputCountingTest {
         println(highScore)
 
     }
+
+    @Test
+    fun shouldCountAllTheCaloriesPerThreeElfs() {
+        val resource = this::class.java.classLoader.getResource("input.data")
+        val dataOfTheElves = resource.readText()
+        val dataPerElve = dataOfTheElves.split("\n\n")
+
+        val highScore = mutableListOf<Int>()
+        for (data in dataPerElve) {
+            val arrayOfCaloriesInText = data.split("\n")
+            val valuesAsNumbers = arrayOfCaloriesInText.map(Integer::parseInt)
+            highScore.add(valuesAsNumbers.sum())
+        }
+
+        highScore.sort()
+        highScore.reverse()
+
+        val sum = highScore[0] + highScore[1] + highScore[2]
+        println(highScore)
+        println(sum)
+
+    }
 }
