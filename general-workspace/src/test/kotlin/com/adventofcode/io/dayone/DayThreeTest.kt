31d5ab479a3e10bf7fb1f8ef7d507221522066e1
Arno Daniel Koehler <arno.koehler@schiphol.nl>
2022-12-07 22:02:08 +0100

    day 3 part two

--- general-workspace/src/test/kotlin/com/adventofcode/io/dayone/DayThreeTest.kt
+++ general-workspace/src/test/kotlin/com/adventofcode/io/dayone/DayThreeTest.kt
@@ -3,30 +3,41 @@ package com.adventofcode.io.dayone
 import org.assertj.core.api.Assertions.assertThat
 import org.junit.jupiter.api.Test
 
-private const val i = 1
-
 class DayThreeTest {
 
     @Test
     fun `should add up to a sum of 157`() {
-        val theLogic = theLogic(getRucksack("verification-set-day-03.data"))
+        val theLogic = theBasicLogic(getRucksack("verification-set-day-03.data"))
         assertThat(theLogic).isEqualTo(157)
     }
 
     @Test
     fun `run the actual calculation`() {
-        val actual = theLogic(getRucksack("actual-input-day-03.data"))
+        val actual = theBasicLogic(getRucksack("actual-input-day-03.data"))
+
+        println(actual)
+    }
+
+    @Test
+    fun `badges should add up to 70`() {
+        val rucksack = getRucksack("verification-set-day-03.data")
+        assertThat(complexLogic(rucksack)).isEqualTo(70)
+    }
+
+    @Test
+    fun `run the actual complexLogic calculation`() {
+        val actual = complexLogic(getRucksack("actual-input-day-03.data"))
 
         println(actual)
     }
 
     private fun getRucksack(s: String): List<String> {
         val resource = this::class.java.classLoader.getResource(s)
-        val rucksackData = resource.readText()
+        val rucksackData = resource!!.readText()
         return rucksackData.split("\n")
     }
 
-    private fun findSameLetter(haystack: String): Char {
+    private fun findSameLetterInOneWord(haystack: String): Char {
         val chunked = haystack.chunked(haystack.length /2)
         println(chunked)
 
@@ -41,16 +52,36 @@ class DayThreeTest {
         return indexOf + 1 // zero based index correction
     }
 
-    private fun theLogic(input: List<String>): Int {
-        val alphabets = CharRange('a','z').toMutableList()
-        val upperCase = CharRange('A','Z').toMutableList()
-        alphabets.addAll(upperCase)
-        val charList = input.map { findSameLetter(it) }
-        val scoreList = charList.map { getCharScore(it, alphabets) }
+    private fun complexLogic(input: List<String>): Int {
+        val groupOfElves = input.chunked(3)
+        println(groupOfElves)
+        val badgesPerGroup = groupOfElves.map { findSameLetterInThreeStrings(it) }
+        val getScoreMap = badgesPerGroup.map { getCharScore(it, getAlphabet()) }
+
+        return getScoreMap.sum()
+    }
+
+    private fun findSameLetterInThreeStrings(input: List<String>): Char {
+        assert(input.size == 3)
+        val charInAllWords = input[0].filter { char -> input[1].contains(char) && input[2].contains(char) }
+        println(charInAllWords)
+        return charInAllWords.first() // if more just select first
+    }
+
+    private fun theBasicLogic(input: List<String>): Int {
+        val charList = input.map { findSameLetterInOneWord(it) }
+        val scoreList = charList.map { getCharScore(it, getAlphabet()) }
 
         println("found chars: $charList")
         println("found scores: $scoreList")
 
         return scoreList.sum()
+    }
+
+    private fun getAlphabet(): MutableList<Char> {
+        val alphabets = CharRange('a', 'z').toMutableList()
+        val upperCase = CharRange('A', 'Z').toMutableList()
+        alphabets.addAll(upperCase)
+        return alphabets
     }
 }
